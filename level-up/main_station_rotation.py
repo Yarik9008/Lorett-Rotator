6a0091aa35983537d9015b6a9e7f0e722e534428
yarik9008 <iaroslav.muhatdinov@yandex.ru>
2022-03-07 13:40:46 +0300

    track

--- level-up/main_station_rotation.py
+++ level-up/main_station_rotation.py
@@ -1,6 +1,9 @@
+from os import name
 from HardwareLorettRotator import *
 from orbital import *
 from threading import Thread
+from pprint import pprint
+from time import sleep
 
 class Main_Lorett_Rotator:
     '''Класс адаптер для организации взаимодействия между отдельными компонентами'''
@@ -9,8 +12,8 @@ class Main_Lorett_Rotator:
 
         self.stationName = 'r8s' 
 
-        #self.path = 'C:/Users/lynx9/YandexDisk/Lorett-Rotator/level-up'
-        self.path = '/home/pi/Lorett-Rotator/level-up'
+        self.path = 'C:/Users/lynx9/YandexDisk/Lorett-Rotator/level-up'
+        #self.path = '/home/pi/Lorett-Rotator/level-up'
 
         self.lat = 55.3970
         self.lon = 55.3970
@@ -35,17 +38,38 @@ class Main_Lorett_Rotator:
 
         self.schedule += self.orbital.getSchedule(24, returnScheduleNameSatellite=True, printTable=True)
 
-    def tracking(track:tuple):
+    def tracking(self,track:tuple):
         '''Функция для отслеживания спутника во время пролета'''
-        pass
+        self.logger.info(f'start tracking satellite {track[0]}')
+        
+        for steps in track[1]:
+            self.logger.debug(f'Go to pozitions: az: {steps[1]} el: {steps[2]}')
+            sleep(1)
 
+        
+        
 
-    def main():
-        pass
+    def sleep_to_next(self, time_sleep:datetime, nameSatellite:str):
+        time_sleep = int(time_sleep.total_seconds())
+        self.logger.info(f'Next satellite {nameSatellite} pass to: {time_sleep} seconds')
+        while time_sleep > 70:
+            sleep(10)
+            time_sleep -= 10
+            self.logger.debug(f'Next satellite {nameSatellite} pass to: {time_sleep} seconds')
+        
             
-
-
-
+    def main(self):
+        while True:
+            # берем следующий пролет
+            satPas = self.schedule[0]
+            self.schedule = self.schedule[1:]
+            # вычисляем время до пролета 
+            sleep_time = satPas[1][0] - datetime.utcnow()
+            #self.sleep_to_next(sleep_time,satPas[0])
+            self.tracking(self.orbital.nextPasses())
+            break
+            
 
 if __name__ == '__main__':
     station = Main_Lorett_Rotator()
+    station.main()
